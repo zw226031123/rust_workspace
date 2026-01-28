b7a44e7ccb8cc2a38dfa00112d628f26b867364c
zhouwen <zhouwen@publink.cn>
2026-01-28 13:37:25 +0800

    elasticsearch

--- rust_elasticsearch/src/bin/elasticsearch_02.rs
+++ rust_elasticsearch/src/bin/elasticsearch_02.rs
@@ -1,9 +1,9 @@
-use elasticsearch::Elasticsearch;
 use elasticsearch::auth::Credentials;
 use elasticsearch::cat::CatIndicesParts;
 use elasticsearch::http::Url;
 use elasticsearch::http::transport::{SingleNodeConnectionPool, TransportBuilder};
-
+use elasticsearch::{Elasticsearch, Error, IndexParts};
+use serde_json::{Value, json};
 #[tokio::main]
 async fn main() {
     let url = Url::parse("http://127.0.0.1:9200").expect("Invalid Elasticsearch URL.");
@@ -15,13 +15,32 @@ async fn main() {
         .build()
         .expect("Failed to build the transport");
     let client = Elasticsearch::new(transport);
+    save(&client).await.expect("save error");
+    indices(&client).await.expect("indices error")
+}
+async fn save(client: &Elasticsearch) -> Result<(), Error> {
+    client
+        .index(IndexParts::IndexId("tweets", "1"))
+        .body(json!({
+            "user": "kimchy",
+            "message": "Hello, Elasticsearch!"
+        }))
+        .send()
+        .await?;
+    Ok(())
+}
 
-    let response = client
+async fn indices(client: &Elasticsearch) -> Result<(), Error> {
+    let body = client
         .cat()
         .indices(CatIndicesParts::Index(&["*"]))
+        .format("json")
         .send()
-        .await
-        .unwrap();
-
-    println!("{:?}", response.text().await);
+        .await?
+        .json::<Value>()
+        .await?;
+    for idx in body.as_array().unwrap() {
+        println!("Index: {}", idx["index"]);
+    }
+    Ok(())
 }
