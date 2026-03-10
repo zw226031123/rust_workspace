e4331b9d13b246b6cf440904476966408bf956e3
zhouwen <zhouwen@publink.cn>
2026-03-10 13:40:32 +0800

    async

--- rust_async/src/bin/tokio_1.rs
+++ rust_async/src/bin/tokio_1.rs
@@ -6,17 +6,19 @@ async fn main() {
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
     loop {
         let (mut socket, _address) = listener.accept().await.unwrap();
-        let (stream_reader, mut stream_writer) = socket.split();
+        tokio::spawn(async move {
+            let (stream_reader, mut stream_writer) = socket.split();
 
-        let mut message = String::new();
-        let mut reader = BufReader::new(stream_reader);
-        loop {
-            let byte_read = reader.read_line(&mut message).await.unwrap();
-            if byte_read == 0 {
-                break;
+            let mut message = String::new();
+            let mut reader = BufReader::new(stream_reader);
+            loop {
+                let byte_read = reader.read_line(&mut message).await.unwrap();
+                if byte_read == 0 {
+                    break;
+                }
+                stream_writer.write_all(message.as_bytes()).await.unwrap();
+                message.clear();
             }
-            stream_writer.write_all(message.as_bytes()).await.unwrap();
-            message.clear();
-        }
+        });
     }
 }
