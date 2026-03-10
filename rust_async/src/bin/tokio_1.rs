5acc49de37eaed5c86683bec153564bb4e2de876
zhouwen <zhouwen@publink.cn>
2026-03-10 14:03:51 +0800

    async

--- rust_async/src/bin/tokio_1.rs
+++ rust_async/src/bin/tokio_1.rs
@@ -1,10 +1,14 @@
 use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
 use tokio::net::TcpListener;
+use tokio::sync::broadcast;
 
 #[tokio::main]
 async fn main() {
     let listener = TcpListener::bind("localhost:8080").await.unwrap();
+    let (tx, _) = broadcast::channel::<String>(10);
     loop {
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
         let (mut socket, _address) = listener.accept().await.unwrap();
         tokio::spawn(async move {
             let (stream_reader, mut stream_writer) = socket.split();
@@ -12,12 +16,20 @@ async fn main() {
             let mut message = String::new();
             let mut reader = BufReader::new(stream_reader);
             loop {
-                let byte_read = reader.read_line(&mut message).await.unwrap();
-                if byte_read == 0 {
-                    break;
+                tokio::select! {
+                    result = reader.read_line(&mut message) => {
+                        if result.unwrap() == 0 {
+                            break;
+                        }
+                        tx.send(message.clone()).unwrap();
+                        message.clear();
+
+                    },
+                    result = rx.recv()=>{
+                        let received_message=result.unwrap();
+                        stream_writer.write_all(received_message.as_bytes()).await.unwrap();
+                    }
                 }
-                stream_writer.write_all(message.as_bytes()).await.unwrap();
-                message.clear();
             }
         });
     }
