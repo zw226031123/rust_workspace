939206cce96a7e52f40ba92304219bc263c59e83
zhouwen <zhouwen@publink.cn>
2026-03-10 13:36:18 +0800

    async

--- rust_async/src/bin/tokio_1.rs
+++ rust_async/src/bin/tokio_1.rs
@@ -3,15 +3,20 @@ use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
-    let listener=TcpListener::bind("localhost:8080").await.unwrap();
-    let (mut socket,_address)=listener.accept().await.unwrap();
-    let (stream_reader,mut stream_writer) = socket.split();
-
-    let mut message=String::new();
-    let mut  reader = BufReader::new(stream_reader);
+    let listener = TcpListener::bind("localhost:8080").await.unwrap();
     loop {
-        reader.read_line(&mut message).await.unwrap();
-        stream_writer.write_all(message.as_bytes()).await.unwrap();
-        message.clear();
+        let (mut socket, _address) = listener.accept().await.unwrap();
+        let (stream_reader, mut stream_writer) = socket.split();
+
+        let mut message = String::new();
+        let mut reader = BufReader::new(stream_reader);
+        loop {
+            let byte_read = reader.read_line(&mut message).await.unwrap();
+            if byte_read == 0 {
+                break;
+            }
+            stream_writer.write_all(message.as_bytes()).await.unwrap();
+            message.clear();
+        }
     }
 }
